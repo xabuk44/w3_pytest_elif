12b97894bb9142136bea33a7f1ab2c3474a288c4
xabuk44 <xabuk44@gmail.com>
2019-02-10 09:37:50 +0300

    simmplified code

--- lib.py
+++ lib.py
@@ -14,14 +14,19 @@ def is_creditabele (age, salary):
     max_age = 60
     min_salary = 30_000
 
-    if age >= min_age:
-        if age < max_age:
-            if salary>= min_salary:
-                return True
 
-            else:
-                return False
-        else:
-            return False
+    # Alt + Ins    что то создать
+    # Alt + Enter   что то поправить
+
+    if age < min_age:
+        return False     #early exit
+    if age > max_age:
+        return False
+    if salary < min_salary:
+        return False
+    # если был Return, то после мы не пойдем
+
+    if min_age <= age < max_age and salary >= min_salary:
+        return True
     else:
         return False
