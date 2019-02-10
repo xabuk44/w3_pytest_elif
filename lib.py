9bf94b015b637aeccbe958c813c84a20d1f026cd
xabuk44 <xabuk44@gmail.com>
2019-02-10 09:23:54 +0300

    is_creditable: complex test

--- lib.py
+++ lib.py
@@ -1,5 +1,27 @@
 def is_creditabele (age, salary):
-    if age > 21:
-        print('continue')
+    # первый тест - всегда True
+    """
+    >>> is_creditabele(30, 40_000)
+    True
+
+    >>> is_creditabele(20, 40_000)
+    False
+
+    >>> is_creditabele(34, 20_000)
+    False
+    """
+    min_age = 21
+    max_age = 60
+    min_salary = 30_000
+
+    if age >= min_age:
+        if age < max_age:
+            if salary>= min_salary:
+                return True
+
+            else:
+                return False
+        else:
+            return False
     else:
         return False
